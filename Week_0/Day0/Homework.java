af134c5ce7a95de950c93ceaeea16e88cc2b09b8
Luvgujjar <luvgujjar2810@gmail.com>
2024-07-11 21:24:31 +0530

    Day_1

--- Week_0/Day0/Homework.java
+++ Week_0/Day0/Homework.java
@@ -1,3 +1,4 @@
+
 public class Homework {
     public static void main(String[] args) {
         System.out.println("Homework DOne !!");
---
+++ Week_0/Day1/Operator.java
@@ -0,0 +1,52 @@
+package Day1;
+
+public class Operator {
+    public static void main(String[] args) {
+        // //1. Airthematic operations
+        // int a=2,b=3;
+        // //+,-,*,%,/,++,--
+        // a=a+b;
+        // b=b+a;
+        // a=a-b;
+        // b=b-a;
+        // ++a; a++; --b; b--;
+
+        // //2. Comparison opertors
+        // // >,<,!=,==,<=,>=
+        // //3.Logical opertors
+        // // &&,//,/
+        // if(!(a>b || a<10) );
+        // //4.Assingment operator
+        // int x=2;
+        // x+=3; //x=x+3;
+        // x-=3; //x=x-3;
+        // x*=3; //x=x*3;
+        // x/=3; //x=x/3;
+        // x%=3; //x=x%3;
+        // //*Homework  Bitwise -> &=,|,^,>>,<<,
+        // //LOOPS : For,While,Do while
+        // //for(int i=0;i<1-;i++){
+        //     //code
+        //     //using a for loop print sum of all numbers bw 1->25
+       
+        //         //FOR LOOP
+        //  //    int sum =0;
+        // //     for (int i=1;i<25;i++){
+        // //         sum=sum+i;
+        // //         System.out.print(sum+" , ");
+        // //     }
+        //     //WHILE LOOP
+        //     //Q PRINT FIRST MULTIPLE OF 2&7 BW 5 TO 15
+
+            int sum=0,  i=5;
+            while(i<15){
+                //Code
+                if (!(i%2==0|i%7==0)){
+                    sum=sum+i;
+                }
+                i++;
+            }
+            System.out.println(i);
+        }
+}
+  
